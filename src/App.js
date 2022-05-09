3e9a44af851a2505e862e4217a2cb60d4769b2f6
MikeCiccia <micheleciccia2@gmail.com>
2022-05-09 16:39:33 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,74 @@
-import { useState } from "react"
-import {initialWorkouts, generateWorkout} from "./Workouts.js"
-import "./App.css"
+import { useState } from "react";
+import { initialWorkouts, generateWorkout } from "./Workouts.js";
+import "./App.css";
 
 function App() {
-  const [workouts, setWorkouts] = useState(initialWorkouts)
+  const [workouts, setWorkouts] = useState(initialWorkouts);
 
   const addNewWorkout = () => {
-    const newWorkout = generateWorkout()
-    console.log("addNewWorkout:", newWorkout)
-  }
-
-  const deleteWorkout = (workout) => {
-    console.log("deleteWorkout:", workout)
-  }
-
-  const completeWorkout = (workout) => {
-    console.log("completeWorkout:", workout)
-  }
-
-  return (
-    <div className="App">
-      <h1>🏋️‍♀️Workout Generator</h1>
-      <button onClick={addNewWorkout}>Add New Workout</button>
-      <ul>
-        {workouts.map((workout, index) => (
-          <li key={index}>
-            <p>
-              {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
-            </p>
-            {!workout.done && 
-              <button onClick={e=>completeWorkout(workout)}>Done</button>}
-            {workout.done && 
-             <p>✅</p>}
-            <button onClick={e=>deleteWorkout(workout)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-      
-    </div>
-  )
+    setWorkouts([...workouts, generateWorkout()]);
+  };
 }
 
-export default App
+const deleteWorkout = (targetWorkout) => {
+  setWorkouts(workouts.filter((workout) => workout !== targetWorkout));
+};
+
+const completeWorkout = (targetWorkout) => {
+  const updatedWorkout = workouts.map((workout) => {
+    if (workout === targetWorkout) {
+      return { ...workout, done: true };
+    }
+    return workout;
+  });
+  setWorkouts(updatedWorkout);
+};
+
+const replaceWorkout = (targetWorkout) => {
+  setWorkouts(
+    workouts.map((workout) => {
+      if (workout === targetWorkout) {
+        return generateWorkout();
+      } else return workout;
+    })
+  );
+};
+return (
+  <div className="App">
+    <h1>🏋️‍♀️Workout Generator</h1>
+    <div className="top-buttons">
+      <button id="add-workout-button" onClick={addNewWorkout}>
+        Add New Workout
+      </button>
+      <div id="checkbox-completed">
+        <input type="checkbox" id="showCompleted" name="showCompleted"></input>
+        <label for="showCompleted">Show completed workouts</label>
+      </div>
+    </div>
+    <ul>
+      {workouts.map((workout, index) => (
+        <li key={index}>
+          <p>
+            {workout.sets}x sets of{" "}
+            <strong>
+              {workout.reps}x{workout.exercise}
+            </strong>{" "}
+            with {workout.rest} seconds rest
+          </p>
+          {!workout.done && (
+            <button onClick={() => completeWorkout(workout)}>Done</button>
+          )}
+          {workout.done && <p>✅</p>}
+          <button onClick={() => deleteWorkout(workout)}>Delete</button>
+          {!workout.done && (
+            <button onClick={() => replaceWorkout(workout)}>
+              Replace {workout.exercise}
+            </button>
+          )}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default App;
